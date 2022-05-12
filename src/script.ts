35b2ff47e86f0ca6448bd1ab47904c5cb22ebe3b
NicolasMontone <montonenicolas01@gmail.com>
2022-05-11 22:36:51 -0300

    fix: get unique video in youtube.

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,6 @@
 function getVideoFromPage() {
   // get the largest video (the current video)
   const videos = Array.from(document.querySelectorAll('video'))
-    .filter((video) => video.readyState != 0)
-    .filter((video) => video.disablePictureInPicture == false)
-    .sort((v1, v2) => {
-      const v1Rect = v1.getClientRects()[0] || { width: 0, height: 0 }
-      const v2Rect = v2.getClientRects()[0] || { width: 0, height: 0 }
-      return v2Rect.width * v2Rect.height - v1Rect.width * v1Rect.height
-    })
 
   if (videos.length === 0) {
     // no videos found just return
