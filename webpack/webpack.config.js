fa78ecfe50dcb1df841ef6fe5ebd41109e1a4f63
Nicolás Montone <83733486+NicolasMontone@users.noreply.github.com>
2022-05-11 22:17:56 -0300

    feat: add pip into video. (#1)
    * chore: update to make google happy :) and change build with webpack.
    
    * fix: remove content and add script file, add logic to handle pip.
    
    * chore: remove commands.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-module.exports = {
-   mode: "production",
-   entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
-      content: path.resolve(__dirname, "..", "src", "content.ts"),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
-   },
-   plugins: [
-      new CopyPlugin({
-         patterns: [{from: ".", to: ".", context: "public"}, {from: ".", to: ".", context: "src/popup"}],
 
-      }),
-   ],
+
+module.exports = {
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, "..", "src", "background.ts"),
+    script: path.resolve(__dirname, "..", "src", "script.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{from: ".", to: ".", context: "public"}, {from: ".", to: ".", context: "src/popup"}],
+
+    }),
+  ],
 };
